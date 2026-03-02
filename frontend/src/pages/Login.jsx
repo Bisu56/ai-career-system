60441922980efb12b6cf719e558ff348048cb2a6
Bisu56 <rajbanshibishal441@gmail.com>
2026-03-02 22:38:52 +0545

    login with api

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,31 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
+import api from "../services/api";
+import { AuthContext } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const { setUser } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const handleLogin = async () => {
+    try {
+      const response = await api.post("/login", {
+        email,
+        password,
+      });
+
+      const token = response.data.token;
+
+      localStorage.setItem("token", token);
+      setUser(response.data.user);
+
+      navigate("/dashboard");
+    } catch (error) {
+      alert("Invalid credentials");
+    }
+  };
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
@@ -25,7 +48,10 @@ export default function Login() {
           onChange={(e) => setPassword(e.target.value)}
         />
 
-        <button className="w-full bg-blue-600 text-white p-2 rounded">
+        <button
+          onClick={handleLogin}
+          className="w-full bg-blue-600 text-white p-2 rounded"
+        >
           Login
         </button>
       </div>
