ccef95832206dcae0d9f7f045565218ae6dc40c6
Bisu56 <rajbanshibishal441@gmail.com>
2026-03-04 10:51:54 +0545

    add api route

--- backend/laravel-api/routes/api.php
+++ backend/laravel-api/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ResumeController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
@@ -8,4 +9,5 @@ Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:api')->group(function () {
     Route::get('/me', [AuthController::class, 'me']);
+    Route::post('/upload-resume', [ResumeController::class, 'upload']);
 });
