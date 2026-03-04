1e2fab293b8d02f46bedfc1737afa8a8c4083973
Bisu56 <rajbanshibishal441@gmail.com>
2026-03-04 10:46:07 +0545

    configure resume model

--- backend/laravel-api/app/Models/Resume.php
+++ backend/laravel-api/app/Models/Resume.php
@@ -2,9 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Resume extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'file_path',
+        'extracted_text',
+        'match_percentage'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
