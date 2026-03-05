211a6dff4e27b7f96a0129000a62c72dbf112957
Bisu56 <rajbanshibishal441@gmail.com>
2026-03-05 22:49:21 +0545

    database

---
+++ backend/ai-service/skills_db.py
@@ -0,0 +1,50 @@
+SKILLS_DATABASE = [
+    "python",
+    "java",
+    "javascript",
+    "react",
+    "laravel",
+    "php",
+    "sql",
+    "machine learning",
+    "data analysis",
+    "docker",
+    "aws",
+    "git",
+    "html",
+    "css",
+    "excel",
+    "linux"
+]
+
+
+CAREER_MAPPING = {
+
+    "Data Analyst": [
+        "python",
+        "sql",
+        "data analysis",
+        "excel"
+    ],
+
+    "Frontend Developer": [
+        "javascript",
+        "react",
+        "html",
+        "css"
+    ],
+
+    "Backend Developer": [
+        "python",
+        "php",
+        "laravel",
+        "sql"
+    ],
+
+    "DevOps Engineer": [
+        "docker",
+        "aws",
+        "linux",
+        "git"
+    ]
+}
